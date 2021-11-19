628bbd82994684f9cc901629061fe4f01b5bb1bb
GraysonRicketts <grayson.ricketts@gmail.com>
2021-11-19 08:06:56 -0600

    Show friend requets

--- src/pages/Authorized/Friends/PendingRequests.tsx
+++ src/pages/Authorized/Friends/PendingRequests.tsx
@@ -1,61 +1,91 @@
 import {
-  Accordion,
-  AccordionDetails,
-  AccordionSummary,
   Box,
   Button,
   CircularProgress,
   List,
   ListItem,
+  ListItemText,
   Typography,
+  ListSubheader,
+  ListItemIcon,
 } from '@mui/material';
+import { Close } from '@mui/icons-material';
 import { useQuery } from 'react-query';
 import { getFriendReqs } from '../../../api/friend';
 
 export const PendingRequests = () => {
   const { data: reqs, isLoading } = useQuery('requests', getFriendReqs);
-  console.log('here');
-  
 
   const handleAccept = () => {
     // TODO
   };
 
-  const handleDelete = () => {
-
-  }
+  const handleDelete = () => {};
 
   return (
-    <Accordion>
-      <AccordionSummary>Requests pending ({reqs?.from.length || 0})</AccordionSummary>
-      <AccordionDetails>
-        {isLoading && <CircularProgress />}
-        {reqs && (
-          <Box>
-            <Box>
-              <List>
-                {reqs.from.map((r) => {
-                  <ListItem
-                    key={r.id + r.createdAt}
-                    secondaryAction={
-                      <>
-                        <Button color="success" onClick={handleAccept}>Accept</Button>
-                        <Button onClick={handleDelete}>Reject</Button>
-                      </>
-                    }
-                  >
-                    <Typography>{r.userFrom}</Typography>
-                  </ListItem>;
-                })}
-              </List>
-            </Box>
-            <Box>
-              <Typography>Waiting to be accepted</Typography>
-            </Box>
-          </Box>
-        )}
-        {/* List of friend requests */}
-      </AccordionDetails>
-    </Accordion>
+    <Box>
+      {isLoading && <CircularProgress />}
+      {reqs && (
+        <List>
+          <ListSubheader>Pending requests ({reqs.to.length})</ListSubheader>
+          {reqs.to.map((r) => {
+            return (
+              <ListItem
+                key={`to_${r.id}`}
+                secondaryAction={
+                  <>
+                    <Button size="small" onClick={handleDelete}>
+                      <Close />
+                    </Button>
+                  </>
+                }
+                disableGutters
+              >
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="success"
+                  onClick={handleAccept}
+                  sx={{mr: 1}}
+                >
+                  Accept
+                </Button>
+                <ListItemText
+                  primary={
+                    <Typography component="p" variant="body1">
+                      {r.userFrom.email}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+          <ListSubheader>Awaiting ({reqs.from.length})</ListSubheader>
+          {reqs.from.map((r) => {
+            return (
+              <ListItem
+                key={`from_${r.id}`}
+                secondaryAction={
+                  <>
+                    <Button size="small" onClick={handleDelete}>
+                      <Close />
+                    </Button>
+                  </>
+                }
+                disableGutters
+              >
+                <ListItemText
+                  primary={
+                    <Typography component="p" variant="body1">
+                      {r.userTo.email}
+                    </Typography>
+                  }
+                />
+              </ListItem>
+            );
+          })}
+        </List>
+      )}
+    </Box>
   );
 };
