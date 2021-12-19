7d27380d02924a06720828aa1f9a01f39fef3061
GraysonRicketts <grayson.ricketts@gmail.com>
2021-12-19 16:00:18 -0600

    one more time

--- src/api/http.ts
+++ src/api/http.ts
@@ -1,10 +1,7 @@
 import axios from 'axios';
-import { API_URI } from '../config';
-
-const uri = API_URI || window.location.origin;
 
 export const httpInstance = axios.create({
-  baseURL: `${uri}/api/v1`,
+  baseURL: `/api/v1`,
 });
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
