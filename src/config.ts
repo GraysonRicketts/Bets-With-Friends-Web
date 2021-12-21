853aafcc84dbfceed5f4d933a86aaf5a4d0ed9fb
GraysonRicketts <grayson.ricketts@gmail.com>
2021-12-20 19:36:21 -0600

    pre tree changes

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,2 @@
 type Environment = 'development' | 'test' | 'staging' | 'production';
 export const NODE_ENV: Environment = process.env.NODE_ENV;
-
-export const API_URI = process.env.REACT_APP_API_URI;
