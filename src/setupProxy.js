820ae98a0f5f856dee1eb80a7e8efa8ca94b279c
GraysonRicketts <grayson.ricketts@gmail.com>
2022-01-17 14:39:03 -0600

    try static proxy

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,17 +1,18 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
-const { API_URL } = require('./config');
 
 // Copied from https://create-react-app.dev/docs/proxying-api-requests-in-development/
 // Apparently has to be js because no ts support, yet
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: API_URL,
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/': '/', // remove base path
-      },
-    })
-  );
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api/': '/', // remove base path
+        },
+      })
+    );
+  }
 };
